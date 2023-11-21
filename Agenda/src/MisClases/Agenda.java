e65fd223dfcdb3fee9af48508dd5f5bad6b4d98a
daniinigogarcia98 <daniinigogarcia98@gmail.com>
2023-11-21 12:11:12 +0100

    agendacompleta

--- Agenda/src/MisClases/Agenda.java
+++ Agenda/src/MisClases/Agenda.java
@@ -1,13 +1,15 @@
 package MisClases;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Agenda {
 	//Atributos
 	DatosPersona[] agenda;
-	
+	Scanner teclado;
 	Agenda(int numEntradas){
 		agenda=new DatosPersona[numEntradas];
+		teclado=new Scanner(System.in);
 	}
 	
 	
@@ -20,10 +22,9 @@ public class Agenda {
 				break;
 			}
 		}
-		if(encontrado==true) {
+		if(encontrado==false) {
 			//He encontrado el elemento
 			//Le pedimos al usuario los datos del nuevo Registro
-			Scanner teclado=new Scanner(System.in);
 			System.out.println("Introduce el Nombre: ");
 			String nombre=teclado.nextLine();
 			System.out.println("Introduce Los Apellidos: ");
@@ -43,26 +44,115 @@ public class Agenda {
 		
 			//Inserto el objeto en la posición Buscada en la agenda
 			agenda[i]=nuevoRegistro;
-			
+			return i;
 		}
 		else {
 			//La agenda estaba llena
 			return-1;
 			
 		}
-		return i;
+		
+		
+		
 	}
 	
 	public void mostrarEntradasAgenda(){
-		
+		System.out.println("**********************************");
 		for(int i=0;i<agenda.length;i++) {
 			if(agenda[i]!=null) {
 				//Muestro los datos de la entrada
 				
-				System.out.println(i+"-"+agenda[i].formatoCorto());
+				System.out.println(i + " - " + agenda[i].formatoCorto());
+				
+			}
+		}
+		System.out.println("**********************************");
+	}
+	
+	
+	public int borrarEntrada() {
+		int entradaBorrada=-1;
+		System.out.println("Introduce el índice de la entrada a borrar");
+		int numero=teclado.nextInt();
+		teclado.nextLine();
+		if(agenda[numero]!=null) {
+			agenda[numero]=null;
+			entradaBorrada=numero;
+			System.out.println("La entrada "+ numero + "se borró correctamente");
+		}
+		else System.err.println("La entrada seleccionada no tiene datos");
+		
+		
+		return entradaBorrada;
+	}
+	
+	public int modificarEntrada() {
+		int entradaModificada =-1;
+		System.out.println("Introduce el índice de la entrada a Modificar");
+		int numero=teclado.nextInt();
+		teclado.nextLine();
+		if(agenda[numero]!=null) {
+			System.out.println("Introduce el Nombre: ("+ agenda[numero].getNombre()+")");
+			String nombre=teclado.nextLine();
+			if(!nombre.isEmpty())
+			  agenda[numero].setNombre(nombre);
+			System.out.println("Introduce Los Apellidos: ("+ agenda[numero].getApellidos()+")");
+			String apellidos=teclado.nextLine();
+			if(!apellidos.isEmpty())
+				  agenda[numero].setApellidos(apellidos);;
+			System.out.println("Introduce la Dirección: ("+ agenda[numero].getDireccion()+")");
+			String direccion=teclado.nextLine();
+			if(!direccion.isEmpty())
+				  agenda[numero].setDireccion(direccion);;
+			System.out.println("Introduce el NúmeroFijo: ("+ agenda[numero].getNumFijo()+")");
+			String fijo=teclado.nextLine();
+			if(!fijo.isEmpty())
+				  agenda[numero].setNumFijo(fijo);
+			System.out.println("Introduce el NúmeroMóvil: ("+ agenda[numero].getNumMovil()+")");
+			String movil=teclado.nextLine();
+			if(!movil.isEmpty())
+				  agenda[numero].setNumMovil(movil);
+			System.out.println("Introduce el Email:("+ agenda[numero].geteMail()+")");
+			String eMail=teclado.nextLine();
+			if(!eMail.isEmpty())
+				  agenda[numero].seteMail(eMail);
+		}
+		else System.err.println("La entrada seleccionada no tiene datos");
+		
+		
+		return entradaModificada;
+	}
+	public void buscarAgenda() {
+		System.out.println("Introduce el término de búsqueda :");
+		String termino=teclado.nextLine();
+		termino=termino.toUpperCase();
+		for(int i=0;i<agenda.length;i++) {
+			if(agenda[i]!=null) {
+				boolean deboMostrar=false;
+				if(agenda[i].getNombre().toUpperCase().contains(termino))
+					deboMostrar=true;
+				else if(agenda[i].getNumMovil().toUpperCase().contains(termino))
+					deboMostrar=true;
+				else if(agenda[i].getNumFijo().toUpperCase().contains(termino))
+					deboMostrar=true;
+				else if(agenda[i].geteMail().toUpperCase().contains(termino))
+					deboMostrar=true;
+				
+				if(deboMostrar==true)
+				System.out.println(i + " - " + agenda[i].formatoCorto());
 			}
 		}
 	}
-	
+	public void compactarAgenda() {
+		DatosPersona[] arAuxiliar=new DatosPersona[agenda.length];
+		int j=0;
+		for(int i=0;i<agenda.length;i++) {
+			if(agenda[i]!=null) {
+				arAuxiliar[j]=agenda[i];
+				j++;
+			}
+		}
+		agenda=arAuxiliar;
+	}
 	//Métodos
 }
--- Agenda/src/MisClases/Aplicacion.java
+++ Agenda/src/MisClases/Aplicacion.java
@@ -1,9 +1,71 @@
 package MisClases;
 
+import java.util.Scanner;
+
 public class Aplicacion {
 
 	public static void main(String[] args) {
 		Agenda miAgenda=new Agenda(100);
+		
+		Scanner teclado=new Scanner(System.in);
+		boolean salir=false;
+		
+		while(salir==false) {
+			System.out.println("1- Mostrar Entradas");
+			System.out.println("2- Añadir Entrada");
+			System.out.println("3- Modificar Entrada");
+			System.out.println("4- Borrar Entrada");
+			System.out.println("5- Buscar Agenda");
+			System.out.println("6- Compactar Agenda");
+			System.out.println("9- Salir");
+			//String opcion=teclado.nextLine();
+			int opcion=teclado.nextInt();
+			switch(opcion) {
+			case 1:
+				miAgenda.mostrarEntradasAgenda();
+				break;
+			case 2:
+				miAgenda.insertarEntrada();
+				break;
+			case 3:
+				miAgenda.modificarEntrada();
+				break;
+			case 4:
+				miAgenda.borrarEntrada();
+				break;
+			case 5:
+				miAgenda.buscarAgenda();
+				break;
+			case 6:
+				miAgenda.compactarAgenda();
+				break;
+			default:
+				salir=true;
+				break;
+			}
+			//if(opcion.equals("1")) {
+				//miAgenda.mostrarEntradasAgenda();
+			//}
+			
+			//if(opcion.equals("2")) {
+				//miAgenda.insertarEntrada();
+			//}
+			//if(opcion.equals("3")) {
+				//miAgenda.modificarEntrada();
+			//}
+			//if(opcion.equals("4")) {
+				//miAgenda.borrarEntrada();
+			//}
+			//if(opcion.equals("5")) {
+				//miAgenda.buscarAgenda();
+			//}
+			//if(opcion.equals("6")) {
+			//miAgenda.compactarAgenda();
+		//}
+			//if(opcion.equals("9")) {
+				//salir=true;
+			//}
+		}
 
 	}
 
